9a5b3141bff9a86cc97e8c78a088239fecfc42ba
AjinkyaBhave <ajinkyaybhave@gmail.com>
2017-08-18 08:10:24 +0530

    Initial commit.

--- detect_vehicles.py
+++ detect_vehicles.py
@@ -1,61 +1,168 @@
-import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-import numpy as np
-import cv2
-import glob
+from classify_vehicles import *
 
-# from skimage.feature import hog
-# from skimage import color, exposure
-# images are divided up into vehicles and non-vehicles
-
-images = glob.glob('*.jpeg')
-cars = []
-notcars = []
-
-for image in images:
-    if 'image' in image or 'extra' in image:
-        notcars.append(image)
+# Define a function to extract features from a single image window
+# This function is very similar to extract_features()
+# just for a single image rather than list of images
+def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
+                        hist_bins=32, orient=9,
+                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
+                        spatial_feat=True, hist_feat=True, hog_feat=True):
+    # 1) Define an empty list to receive features
+    img_features = []
+    # 2) Apply color conversion if other than 'RGB'
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+        elif color_space == 'LAB':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
     else:
-        cars.append(image)
+        feature_image = np.copy(img)
+    # 3) Compute spatial features if flag is set
+    if spatial_feat == True:
+        spatial_features = bin_spatial(feature_image, size=spatial_size)
+        # 4) Append features to list
+        img_features.append(spatial_features)
+    # 5) Compute histogram features if flag is set
+    if hist_feat == True:
+        hist_features = color_hist(feature_image, nbins=hist_bins)
+        # 6) Append features to list
+        img_features.append(hist_features)
+    # 7) Compute HOG features if flag is set
+    if hog_feat == True:
+        if hog_channel == 'ALL':
+            hog_features = []
+            for channel in range(feature_image.shape[2]):
+                hog_features.extend(get_hog_features(feature_image[:, :, channel],
+                                                     orient, pix_per_cell, cell_per_block,
+                                                     vis=False, feature_vec=True))
+        else:
+            hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
+                                            pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+        # 8) Append features to list
+        img_features.append(hog_features)
+
+    # 9) Return concatenated array of features
+    return np.concatenate(img_features)
+
+# Define a function that takes an image,
+# start and stop positions in both x and y,
+# window size (x and y dimensions),
+# and overlap fraction (for both x and y)
+def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
+                 xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
+    # If x and/or y start/stop positions not defined, set to image size
+    if x_start_stop[0] == None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] == None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] == None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] == None:
+        y_start_stop[1] = img.shape[0]
+    # Compute the span of the region to be searched
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0] * (1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1] * (1 - xy_overlap[1]))
+    # Compute the number of windows in x/y
+    nx_buffer = np.int(xy_window[0] * (xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1] * (xy_overlap[1]))
+    nx_windows = np.int((xspan - nx_buffer) / nx_pix_per_step)
+    ny_windows = np.int((yspan - ny_buffer) / ny_pix_per_step)
+    # Initialize a list to append window positions to
+    window_list = []
+    # Loop through finding x and y window positions
+    # Note: you could vectorize this step, but in practice
+    # you'll be considering windows one by one with your
+    # classifier, so looping makes sense
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs * nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys * ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
+
+            # Append window position to list
+            window_list.append(((startx, starty), (endx, endy)))
+    # Return the list of windows
+    return window_list
 
 
-# Define a function to return some characteristics of the dataset
-def data_look(car_list, notcar_list):
-    data_dict = {}
-    # Define a key in data_dict "n_cars" and store the number of car images
-    data_dict["n_cars"] = len(car_list)
-    # Define a key "n_notcars" and store the number of notcar images
-    data_dict["n_notcars"] = len(notcar_list)
-    # Read in a test image, either car or notcar
-    img = mpimg.imread(cars[0])
-    # Define a key "image_shape" and store the test image shape 3-tuple
-    data_dict["image_shape"] = img.shape
-    # Define a key "data_type" and store the data type of the test image.
-    data_dict["data_type"] = img.dtype
-    # Return data_dict
-    return data_dict
+# Define a function to draw bounding boxes
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+
+# Define a function you will pass an image
+# and the list of windows to be searched (output of slide_windows())
+def search_windows(img, windows, clf, scaler, color_space='RGB',
+                   spatial_size=(32, 32), hist_bins=32,
+                   hist_range=(0, 256), orient=9,
+                   pix_per_cell=8, cell_per_block=2,
+                   hog_channel=0, spatial_feat=True,
+                   hist_feat=True, hog_feat=True):
+    # 1) Create an empty list to receive positive detection windows
+    on_windows = []
+    # 2) Iterate over all windows in the list
+    for window in windows:
+        # 3) Extract the test window from original image
+        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))
+        # 4) Extract features for that window using single_img_features()
+        features = single_img_features(test_img, color_space=color_space,
+                                       spatial_size=spatial_size, hist_bins=hist_bins,
+                                       orient=orient, pix_per_cell=pix_per_cell,
+                                       cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                       hist_feat=hist_feat, hog_feat=hog_feat)
+        # 5) Scale extracted features to be fed to classifier
+        test_features = scaler.transform(np.array(features).reshape(1, -1))
+        # 6) Predict using your classifier
+        prediction = clf.predict(test_features)
+        # 7) If positive (prediction == 1) then save the window
+        if prediction == 1:
+            on_windows.append(window)
+    # 8) Return windows for positive detections
+    return on_windows
 
 
-data_info = data_look(cars, notcars)
+img = cv2.imread('bbox-example-image.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+draw_image = np.copy(img)
 
-print('Your function returned a count of',
-      data_info["n_cars"], ' cars and',
-      data_info["n_notcars"], ' non-cars')
-print('of size: ', data_info["image_shape"], ' and data type:',
-      data_info["data_type"])
-# Just for fun choose random car / not-car indices and plot example images
-car_ind = np.random.randint(0, len(cars))
-notcar_ind = np.random.randint(0, len(notcars))
+# Uncomment the following line if you extracted training
+# data from .png images (scaled 0 to 1 by mpimg) and the
+# image you are searching is a .jpg (scaled 0 to 255)
+# image = image.astype(np.float32)/255
 
-# Read in car / not-car images
-car_image = mpimg.imread(cars[car_ind])
-notcar_image = mpimg.imread(notcars[notcar_ind])
+windows = slide_window(img, x_start_stop=[None, None], y_start_stop=y_start_stop,
+                       xy_window=(96, 96), xy_overlap=(0.5, 0.5))
 
-# Plot the examples
-fig = plt.figure()
-plt.subplot(121)
-plt.imshow(car_image)
-plt.title('Example Car Image')
-plt.subplot(122)
-plt.imshow(notcar_image)
-plt.title('Example Not-car Image')
+hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space,
+                             spatial_size=spatial_size, hist_bins=hist_bins,
+                             orient=orient, pix_per_cell=pix_per_cell,
+                             cell_per_block=cell_per_block,
+                             hog_channel=hog_channel, spatial_feat=spatial_feat,
+                             hist_feat=hist_feat, hog_feat=hog_feat)
+
+window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
+
+plt.imshow(window_img)
+
+
