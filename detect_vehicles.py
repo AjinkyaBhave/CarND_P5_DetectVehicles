b0206fcb4b4b879cc6f46c024472886b60e5f0d0
AjinkyaBhave <ajinkyaybhave@gmail.com>
2017-08-28 18:04:17 +0530

    Testing on video images.

--- detect_vehicles.py
+++ detect_vehicles.py
@@ -5,11 +5,6 @@ from classify_vehicles import *
 from scipy.ndimage.measurements import label
 from moviepy.editor import VideoFileClip
 
-video_input   = 'project_video.mp4'
-video_output  = 'project_video_output.mp4'
-img_dir       = 'test_images/'
-video_img_dir =  img_dir+'test_video/'
-
 def track_vehicles(img, visualise=False):
     # Image copy to draw detected vehicle boxes after heat maps
     img_draw = np.copy(img)
@@ -17,9 +12,8 @@ def track_vehicles(img, visualise=False):
     img_boxes = np.copy(img)
     # Heat map to combine multiple scale detections
     img_heat = np.zeros_like(img[:, :, 0]).astype(np.float)
-    # Minumum number of times a pixel is present in a bounding box set to accept detection
-    heat_thresh = 2
-
+    # List to store detected windows at all image scales
+    all_detected_windows = []
     # Uncomment the following line if you extracted training
     # data from .png images (scaled 0 to 1 by mpimg) and the
     # image you are searching is a .jpg (scaled 0 to 255)
@@ -33,20 +27,20 @@ def track_vehicles(img, visualise=False):
         y_stop  = y_start_stop[i][1]
         cells_per_xstep = cells_xstep_list[i]
         cells_per_ystep = cells_ystep_list[i]
-        #print(scale, x_start, x_stop, y_start, y_stop)
+
         detected_windows = find_vehicles(img, scale, cells_per_xstep, cells_per_ystep,
-                                         x_start, x_stop, y_start, y_stop)
+                                         x_start, x_stop, y_start, y_stop, visualise=visualise  )
         all_detected_windows.extend(detected_windows)
 
     img_heat=create_heatmap(img_heat,heat_thresh, all_detected_windows)
     labels = label(img_heat)
     t2 = time.time()
-    #print('Detection time: ', round(t2-t1,2))
-    #print(labels[1], 'cars found')
     img_draw  = draw_labeled_boxes(img_draw, labels)
     img_boxes = draw_boxes(img_boxes, all_detected_windows)
 
     if visualise == True:
+        print('Detection time: ', round(t2 - t1, 2))
+        print(labels[1], 'Vehicles found')
         fig = plt.figure()
         plt.subplot(131)
         plt.imshow(img_boxes)
@@ -63,16 +57,13 @@ def track_vehicles(img, visualise=False):
     return img_draw
 
 # Define a single function that can extract features using hog sub-sampling and make predictions
-def find_vehicles(img, scale, cells_per_xstep, cells_per_ystep, x_start, x_stop,y_start, y_stop):
+def find_vehicles(img, scale, cells_per_xstep, cells_per_ystep, x_start, x_stop,y_start, y_stop, visualise=False):
     # Convert image to colour space used in SVM classifier training
     img_conv = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
     img_search = img_conv[y_start:y_stop, x_start:x_stop, :]
     img_local = np.copy(img)
-
-    if scale != 1:
-        img_search = cv2.resize(img_search, (np.int(img_search.shape[1] / scale),
+    img_search = cv2.resize(img_search, (np.int(img_search.shape[1] / scale),
                                              np.int(img_search.shape[0] / scale)))
-
     if hog_channel == 'ALL':
         ch1 = img_search[:, :, 0]
         ch2 = img_search[:, :, 1]
@@ -96,9 +87,11 @@ def find_vehicles(img, scale, cells_per_xstep, cells_per_ystep, x_start, x_stop,
     nxsteps = (nxblocks - nwinblocks) // cells_per_xstep
     nysteps = (nyblocks - nwinblocks) // cells_per_ystep
 
+    if visualise == True:
+        print(scale, x_start, x_stop, y_start, y_stop)
+        #print('nxblocks: ', nxblocks, ' nxsteps: ', nxsteps,  )
     # Initialize a list to append window positions to
     window_list = []
-
     for xb in range(nxsteps):
         for yb in range(nysteps):
             # Define an empty list to receive features
@@ -134,8 +127,10 @@ def find_vehicles(img, scale, cells_per_xstep, cells_per_ystep, x_start, x_stop,
             test_prediction = svc.predict(test_features)
 
             if test_prediction == 1:
-                #print('Confidence: ', svc.decision_function(test_features))
-                if svc.decision_function(test_features) > 1.0:
+                if visualise == True:
+                    print('Confidence: ', svc.decision_function(test_features))
+                if svc.decision_function(test_features) > SVC_CONF_THRESH:
+                    #print('Detected Confidence: ', svc.decision_function(test_features))
                     win_scaled = np.int(window * scale)
                     startx = np.int(xleft * scale) + x_start
                     starty = np.int(ytop * scale) + y_start
@@ -143,21 +138,36 @@ def find_vehicles(img, scale, cells_per_xstep, cells_per_ystep, x_start, x_stop,
                     endy = starty + win_scaled
                     # Append window position to list
                     window_list.append(((startx, starty), (endx, endy)))
+                    if visualise == True:
+                        if scale == 1:
+                            colour_tuple = (0, 0, 255)
+                        elif scale == 1.5:
+                            colour_tuple = (255, 0, 0)
+                        else:
+                            colour_tuple = (0, 255, 0)
+                        cv2.rectangle(img_local, (startx, starty), (endx, endy), colour_tuple, 3)
+                        plt.imshow(img_local)
+                        plt.show()
 
-            '''if scale ==1:
-                colour_tuple = (0, 0, 255)
-            elif scale == 1.5:
-                colour_tuple = (255, 0, 0)
-            else:
-                colour_tuple = (0, 255, 0)
-            win_scaled = np.int(window * scale)
-            startx = np.int(xleft * scale) + x_start
-            starty = np.int(ytop * scale) + y_start
-            endx = startx + win_scaled
-            endy = starty + win_scaled
-            cv2.rectangle(img_local, (startx, starty),(endx, endy), colour_tuple, 3)
-    plt.imshow(img_local)
-    plt.show()
+            '''if visualise == True and TEST_ON_VIDEO == False:
+                if scale ==1:
+                    colour_tuple = (0, 0, 255)
+                elif scale == 1.5:
+                    colour_tuple = (255, 0, 0)
+                else:
+                    colour_tuple = (0, 255, 0)
+                win_scaled = np.int(window * scale)
+                startx = np.int(xleft * scale) + x_start
+                starty = np.int(ytop * scale) + y_start
+                endx = startx + win_scaled
+                endy = starty + win_scaled
+                cv2.rectangle(img_local, (startx, starty),(endx, endy), colour_tuple, 3)
+            '''
+    '''if visualise == True and TEST_ON_VIDEO == False:
+        plt.imshow(img_local)
+        plt.show()
+        plt.imshow(img_search)
+        plt.show()
     '''
     return window_list
 
@@ -197,26 +207,35 @@ def draw_labeled_boxes(img_draw, labels):
     return img_draw
 
 if __name__ == '__main__':
+    # Paths to test images and videos
+    video_input = 'test_video.mp4'
+    video_output = 'test_video_output.mp4'
+    img_dir = 'test_images/'
+    video_img_dir = img_dir + 'test_video/'
+
+    # Scales to search for vehicle features in image
+    scale_list = [2, 1.5, 1]
+    # Region in x and y to search in slide_window based on scale
+    x_start_stop = [(0, 1280), (300, 1280), (300, 1280)]
+    y_start_stop = [(400, 700), (400, 600), (400, 560)]
+    # Overlap in cells per step x and y
+    cells_xstep_list = [3, 2, 3]
+    cells_ystep_list = [2, 2, 2]
+
+    # Classifier confidence above which detection is true
+    SVC_CONF_THRESH = 0.1
+    # Minumum number of times a pixel is present in a bounding box set to accept detection
+    heat_thresh = 1
     # Load pre-trained SVM classifier model
     svc = joblib.load(svm_model_path)
     # Load pre-trained per-column scaler
     X_scaler = joblib.load(scaler_model_path)
     print('Load SVM and Scaler')
 
-    # Scales to search for vehicle features in image
-    scale_list = [2,1.5,1]
-    # Region in x and y to search in slide_window based on scale
-    x_start_stop = [(0, 1280), (100,1280), (200,1280)]#[(0, 1200),(200, 850),(400,700)]
-    y_start_stop = [(500, 700),(400, 600),(400, 560) ]#[(400, 700),(400, 600),(470, 510) ]
-    # Overlap in cells per step x and y
-    cells_xstep_list = [2, 2, 3]
-    cells_ystep_list = [1, 2, 2]
-    # List to store detected windows at all image scales
-    all_detected_windows = []
     # Run on video file if true else run on test images
-    TEST_ON_VIDEO = 1
+    TEST_ON_VIDEO = True
 
-    if TEST_ON_VIDEO:
+    if TEST_ON_VIDEO == True:
         # Video is at 25 FPS
         clip = VideoFileClip(video_input)
         clip_output = clip.fl_image(track_vehicles)  # NOTE: this function expects color images!!
@@ -233,11 +252,9 @@ if __name__ == '__main__':
                 clip.save_frame(video_img_file, vt)
 
         # Read camera frames from disk
-        # img_files = glob.glob(video_img_dir+'video*.jpg')
-        img_files = glob.glob(img_dir + '*.jpg')
+        img_files = glob.glob(video_img_dir+'video*.jpg')
+        #img_files = glob.glob(img_dir + '*.jpg')
         for img_file in img_files:
             img = cv2.imread(img_file)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             track_vehicles(img, visualise=True)
-
-
---
+++ sliding_windows.py
@@ -0,0 +1,129 @@
+# Define a function to extract features from a single image window
+# This function is very similar to extract_features()
+# just for a single image rather than list of images
+def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
+                        hist_bins=32, orient=9,
+                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
+                        use_spatial=True, use_hist=True, use_hog=True):
+    # 1) Define an empty list to receive features
+    img_features = []
+    # 2) Apply color conversion if other than 'RGB'
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+        elif color_space == 'Lab':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+    else:
+        feature_image = np.copy(img)
+    # 3) Compute spatial features if flag is set
+    if use_spatial == True:
+        spatial_features = bin_spatial(feature_image, size=spatial_size)
+        # 4) Append features to list
+        img_features.append(spatial_features)
+    # 5) Compute histogram features if flag is set
+    if use_hist == True:
+        hist_features = color_hist(feature_image, nbins=hist_bins)
+        # 6) Append features to list
+        img_features.append(hist_features)
+    # 7) Compute HOG features if flag is set
+    if use_hog == True:
+        if hog_channel == 'ALL':
+            hog_features = []
+            for channel in range(feature_image.shape[2]):
+                hog_features.extend(get_hog_features(feature_image[:, :, channel],
+                                                     orient, pix_per_cell, cell_per_block,
+                                                     vis=False, feature_vec=True))
+        else:
+            hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
+                                            pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+        # 8) Append features to list
+        img_features.append(hog_features)
+
+    # 9) Return concatenated array of features
+    return np.concatenate(img_features)
+
+# Define a function that takes an image,
+# start and stop positions in both x and y,
+# window size (x and y dimensions),
+# and overlap fraction (for both x and y)
+def slide_window(img, x_start=None, x_stop=None, y_start=None, y_stop=None,
+                 xy_window=(train_img_width, train_img_height), xy_overlap=(0.5, 0.5)):
+    # If x and/or y start/stop positions not defined, set to image size
+    if x_start == None:
+        x_start = 0
+    if x_stop == None:
+        x_stop = img.shape[1]
+    if y_start == None:
+        y_start = 0
+    if y_stop == None:
+        y_stop = img.shape[0]
+    # Compute the span of the region to be searched
+    xspan = x_stop - x_start
+    yspan = y_stop - y_start
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0] * (1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1] * (1 - xy_overlap[1]))
+    # Compute the number of windows in x/y
+    nx_buffer = np.int(xy_window[0] * (xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1] * (xy_overlap[1]))
+    nx_windows = np.int((xspan - nx_buffer) / nx_pix_per_step)
+    ny_windows = np.int((yspan - ny_buffer) / ny_pix_per_step)
+    # Initialize a list to append window positions to
+    window_list = []
+    # Loop through finding x and y window positions
+    # Note: you could vectorize this step, but in practice
+    # you'll be considering windows one by one with your
+    # classifier, so looping makes sense
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs * nx_pix_per_step + x_start
+            endx = startx + xy_window[0]
+            starty = ys * ny_pix_per_step + y_start
+            endy = starty + xy_window[1]
+
+            # Append window position to list
+            window_list.append(((startx, starty), (endx, endy)))
+    # Return the list of windows
+    return window_list
+
+# Define a function you will pass an image
+# and the list of windows to be searched (output of slide_windows())
+def search_windows(img, windows, clf, scaler, color_space='RGB',
+                   spatial_size=(32, 32), hist_bins=32,
+                   hist_range=(0, 256), orient=9,
+                   pix_per_cell=8, cell_per_block=2,
+                   hog_channel=0, use_spatial=True,
+                   use_hist=True, use_hog=True):
+    # Create an empty list to receive positive detection windows
+    on_windows = []
+    # Iterate over all windows in the list
+    for window in windows:
+        # Extract the test window from original image
+        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]],
+                              (train_img_height, train_img_width))
+        # Extract features for that window using single_img_features()
+        features = single_img_features(test_img, color_space=color_space,
+                                       spatial_size=spatial_size, hist_bins=hist_bins,
+                                       orient=orient, pix_per_cell=pix_per_cell,
+                                       cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel, use_spatial=use_spatial,
+                                       use_hist=use_hist, use_hog=use_hog)
+        # Scale extracted features to be fed to classifier
+        test_features = scaler.transform(np.array(features).reshape(1, -1))
+        # Predict using classifier
+        prediction = clf.predict(test_features)
+        print('Confidence: ', clf.decision_function(test_features))
+        # If positive (prediction == 1) then save the window
+        if prediction == 1:
+            on_windows.append(window)
+    # Return windows for positive detections
+    return on_windows
